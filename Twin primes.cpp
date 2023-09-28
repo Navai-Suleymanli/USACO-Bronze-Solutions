c26fdb0d859d96a99350b658e83892151d36a350
Navai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2023-09-28 14:33:30 +0300

    Update Twin primes.cpp

--- Twin primes.cpp
+++ Twin primes.cpp
@@ -1,52 +1,56 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
-bool isPrime(int n){
-    if(n<1){
+bool isPrime(int num){
+    if(num <=1){
         return false;
     }
-    if(n<=3){
+    if(num<=3){
         return true;
     }
-    for(int i=5; i*i<n; i+=6){
-        if(i%2==0 || i%3==0){
+    if(num %2==0 || num % 3==0){
+        return false;
+    }
+
+    for(int i = 5; i*i<num; i+=6){
+        if(num % i==0 || num % (i+2) == 0){
             return false;
         }
     }
     return true;
 }
 
-bool Has5(int n){
-    while(n>0){
-        if(n %10==5){
+bool hasDigit5(int num){
+    while(num >0){
+        if(num % 10 == 5){
             return true;
         }
-        n/=10;
+        num /= 10;
     }
     return false;
 }
 
 
 
-int main() {
-    int twinPrimeCount = 0;
-    int number =3;
 
-    while(twinPrimeCount <60){
+int main() {
+    // Write C++ code here
+    int twinPrimeCount = 0;
+    int number = 3;
+
+    while(twinPrimeCount < 60){
         if(isPrime(number) && isPrime(number+2)){
             twinPrimeCount ++;
-             cout<<"Twin Prime "<< twinPrimeCount << "("<<number<<" ;" << number +2<<" )"<<endl;
-             int numberBetween =  number+1;
-             if(Has5(numberBetween)){
-                 cout<<"The number between "<<number<<" and "<<number+2<<"has 5!"<<endl;
-             }
-             
-             number+=2;
+            cout<<"Twin Prime "<< twinPrimeCount << "("<<number<<" ;" << number +2<<" )"<<endl;
+
+            int numBetween = number +1;
+            if(hasDigit5(numBetween)){
+            cout<<"Number between the primes is: " << numBetween<< "and iut contains 5" << endl;
+            }
         }
+        number +=2;
     }
-    
-    cout<<"60th twin prime is: "<<number-2<<" "<<number<<endl;
+
 
     return 0;
 }
